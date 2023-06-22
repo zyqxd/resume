b6da0b6e404ce80114033281567b0a991f94d7c9
David Zhang <zyq.david@gmail.com>
2023-06-22 16:24:29 -0400

    Serve default resume

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile("./public/resume.html");
 });
 
 app.listen(port, () => {
